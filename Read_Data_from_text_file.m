693ba96804e46296fc2d5d79043dc5069fe6d10c
Amr Mohamed <133386340+amrmohamed230@users.noreply.github.com>
2023-05-13 23:21:00 +0300

    Delete Read_Data_from_text_file.m

--- Read_Data_from_text_file.m
+++
@@ -1,5 +0,0 @@
-filename='DatatoRead.txt';
-fileid=fopen(filename,'r'); % r: to open the file for reading
-z=fscanf(fileid,'%c') % '%c' to read the special characters and spaces in the file
-contains(z,'Senior')
-fclose(fileid); %to close the file
